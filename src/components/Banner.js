b18563d0f73e9bac788a38a27663a9d6577747d9
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2023-08-01 10:09:56 -0700

    I created a contact me section.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import styles from '../css/Banner.module.css';
+import { ReactComponent as GitHubLogo } from '../img/github.svg';
+import { ReactComponent as LinkedInLogo } from '../img/linkedin.svg';
+import { ReactComponent as DevToLogo } from '../img/devto.svg';
 
 const Banner = () => {
     return (
         <div className={styles.banner}>
-            <h2 className={styles.text}>Let's Get In Touch!</h2>
+            <h2 className={styles.text}>Let's Be Internet BFFs!</h2>
+            <div className={styles.logos}>
+                <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+                    <GitHubLogo className={styles.logo}/>
+                </a>
+                <a href="https://www.linkedin.com/in/yourusername/" target="_blank" rel="noopener noreferrer">
+                    <LinkedInLogo className={styles.logo}/>
+                </a>
+                <a href="https://dev.to/yourusername" target="_blank" rel="noopener noreferrer">
+                    <DevToLogo className={styles.logo}/>
+                </a>
+            </div>
         </div>
     );
 };
 
 export default Banner;
+
